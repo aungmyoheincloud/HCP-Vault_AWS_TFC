c7fcf0760164abeab9068ab72a56d70566384f2d
Luhein <aungmyohei.cloud@gmail.com>
2024-08-31 11:03:23 +0700

    last run hcp vualt

--- main.tf
+++ main.tf
@@ -1,35 +1,17 @@
-output "hvn_id" {
-  description = "HCP HVN ID"
-  value = hcp_hvn.vault_hvn
+resource "hcp_hvn" "vault_hvn" {
+  hvn_id         = var.vault_hvn
+  cloud_provider = var.cloud_provider
+  region         = var.region
+  cidr_block     = var.cluster_cider
 }
 
-output "vault_id" {
-  description = "HCP Vault Cluster ID"
-  value = hcp_vault_cluster.vault_cluster.id
+resource "hcp_vault_cluster" "vault_cluster" {
+  cluster_id = var.vault_cluster_id
+  hvn_id     = hcp_hvn.vault_hvn.hvn_id
+  tier       = var.tier_type
+  public_endpoint = true
 }
 
-output "vault_namespace" {
-  description = "HCP Vault Cluster Namespace"
-  value = hcp_vault_cluster.vault_cluster.namespace
-}
-
-output "vault_public_endpoint_url" {
-  description = "HCP Vault Cluster vault_public_endpoint_url"
-  value = hcp_vault_cluster.vault_cluster.vault_public_endpoint_url
-}
-
-output "vault_version" {
-  description = "HCP Vault Cluster vault_version"
-  value = hcp_vault_cluster.vault_cluster.vault_version
-}
-
-output "vault_admin_token_id" {
-  description = "HCP Vault Admin Token ID"
-  value = hcp_vault_cluster_admin_token.vault_admin_token.id
-}
-
-output "vault_admin_token" {
-  description = "HCP Vault Admin Token"
-  value = hcp_vault_cluster_admin_token.vault_admin_token.token
-  sensitive = true
+resource "hcp_vault_cluster_admin_token" "vault_admin_token" {
+  cluster_id = hcp_vault_cluster.vault_cluster.cluster_id
 }
