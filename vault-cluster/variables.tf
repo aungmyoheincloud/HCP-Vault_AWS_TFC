d2390d4f55954d75ab20bc4378accba55d0c7c54
Luhein <aungmyohei.cloud@gmail.com>
2024-08-31 10:56:20 +0700

    new run

--- vault-cluster/variables.tf
+++ vault-cluster/variables.tf
@@ -19,7 +19,7 @@ variable "cluster_cider" {
 variable "vault_cluster_id" {
     description = "Cluster Name"
     type = string
-    default = "hellocloud-hcp-vault-cluster"
+    default = "hcp-vault-cluster"
 }
 
 variable "vault_hvn" {
