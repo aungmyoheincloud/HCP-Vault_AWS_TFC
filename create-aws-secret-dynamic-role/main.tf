d680dc3da5601352d05c13d9b2a521df5d9ab233
Luhein <aungmyohei.cloud@gmail.com>
2024-10-04 14:48:33 +0700

    modified codes

--- create-aws-secret-dynamic-role/main.tf
+++ create-aws-secret-dynamic-role/main.tf
@@ -1,3 +1,4 @@
+
 resource "vault_aws_secret_backend" "aws" {
   access_key                = aws_iam_access_key.vault_root.id
   secret_key                = aws_iam_access_key.vault_root.secret
@@ -13,15 +14,11 @@ resource "vault_aws_secret_backend_role" "role" {
   credential_type = "iam_user"
 
   policy_arns = ["arn:aws:iam::aws:policy/AdministratorAccess"]
+
 }
 
-# resource "time_sleep" "wait_30_seconds" {
-#   depends_on      = [vault_aws_secret_backend_role.role]
-#   create_duration = "30s"
-# }
-
+# generally, these blocks would be in a different module
 data "vault_aws_access_credentials" "creds" {
-  # depends_on = [time_sleep.wait_30_seconds]
   backend = vault_aws_secret_backend.aws.path
   role    = vault_aws_secret_backend_role.role.name
 }
