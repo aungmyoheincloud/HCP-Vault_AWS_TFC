9e57edaf7a32ddde8a562c1d85704e52f5e6e7b6
Luhein <aungmyohei.cloud@gmail.com>
2024-10-04 12:43:39 +0700

    modified codes for jwt auth method

--- Terraform-Vault-JWT-Auth-Backend-Setup/outputs.tf
+++ Terraform-Vault-JWT-Auth-Backend-Setup/outputs.tf
@@ -1,10 +1,20 @@
-output "openid_claims" {
-  description = "OpenID Claims for trust relationship"
-  value       = vault_jwt_auth_backend_role.tfc_role.bound_claims
+# output "openid_claims" {
+#   description = "OpenID Claims for trust relationship"
+#   value       = vault_jwt_auth_backend_role.tfc_role.bound_claims
+# }
+
+# output "run_role" {
+#   description = "Name of the vault role for trust relationship"
+#   value       = vault_jwt_auth_backend_role.tfc_role.role_name
+# }
+
+output "admin_role_name" {
+  value       = vault_jwt_auth_backend_role.example.role_name
+  description = "Admin Role Name"
 }
 
-output "run_role" {
-  description = "Name of the vault role for trust relationship"
-  value       = vault_jwt_auth_backend_role.tfc_role.role_name
+output "admin_bound_claim" {
+  value       = vault_jwt_auth_backend_role.example.bound_claims
+  description = "Admin Bound Clamins Name"
 }
 
